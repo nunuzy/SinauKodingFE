aaef704d63a85d09ad709e7938a401a7c3dad891
nunuzy <fnukhi@gmail.com>
2022-08-19 09:59:31 +0700

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,520 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="Description" content="Enter your description here" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/4.6.0/cosmo/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
+    />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!-- CSS only -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx"
+      crossorigin="anonymous"
+    />
+    <!-- JavaScript Bundle with Popper -->
+
+    <title>AXA Life Indonesia</title>
+    <link rel="icon" type="" href="assets/img/logo.png" />
+  </head>
+
+  <body>
+    <nav class="navbar" style="background-color: #26468f; padding: 10px">
+      <!-- Navbar content -->
+    </nav>
+    <header>
+      <nav class="navbar" style="background-color: #26468f; padding: 1.7px">
+        <!-- Navbar content -->
+      </nav>
+      <nav id="navig_bar" class="navbar navbar-default">
+        <div class="container">
+          <div class="col-sm-3 col-md-3">
+            <div class="logo">
+              <img
+                src="../assets/img/logo.png"
+                width="100px"
+                height="100px"
+                alt=""
+              />
+            </div>
+          </div>
+          <button class="btn btn-primary" type="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="12"
+              height="10"
+              fill="currentColor"
+              class="bi bi-chevron-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+              />
+            </svg>
+            Kembali ke Pilihkartu
+          </button>
+        </div>
+      </nav>
+      <h4 class="smalltitle">
+        DAPATKAN ASURANSI GRATIS DARI AXA LIFE INDONESIA
+      </h4>
+      <h1 class="bigtitle">UNTUK PEMEGANG KARTU KREDIT</h1>
+    </header>
+
+    <div class="pembungkus">
+      <img src="../assets/img/slides.png" class="img-fluid" alt="..." />
+      <button
+        id="btn"
+        class="btn btn-primary btn-lg fw-bolder d-grid col-2 mx-auto"
+        type="button"
+      >
+        GRATIS!
+      </button>
+    </div>
+
+    <h3 class="fw-bold">
+      Keuntungan mendaftar
+      <medium class="text">AXA Life Indonesia</medium>
+      di Pilihkartu
+    </h3>
+    <div class="container text-right">
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-lg-6 fw-semibold">
+          <img
+            src="../assets/img/icon_1.png"
+            class="col-md-2 float-md-end mb-2 ms-md-0"
+            alt="..."
+          />
+          Diberikan gratis secara cuma-cuma tanpa premi, <br />
+          <b class="premi"> PREMI: RP.0,- (NOL RUPIAH)</b>
+        </div>
+        <div
+          class="col-sm-5 offset-sm-2 col-md-6 offset-md-0 text-md-left fw-semibold"
+        >
+          <img
+            src="../assets/img/icon_4.png"
+            class="col-md-2 float-md-start mb-2 ms-md-0"
+            alt="..."
+          />
+          Pendaftan yang lolos verifikasi Tim AXA berhak mendapat perlindungan
+          Asuransi kecelakaan diri
+          <strong>selama 3 bulan.</strong>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-lg-6 fw-semibold">
+          <img
+            src="../assets/img/icon_2.png"
+            class="col-md-2 float-md-end mb-2 ms-md-0"
+            alt="..."
+          />
+          Santunan meninggal dunia akibat kecelakaan
+          <p>
+            sebesar (<strong>Rp 25.000.000,-</strong>)(dua puluh lima juta
+            rupiah).
+          </p>
+        </div>
+        <div
+          class="col-sm-5 offset-sm-2 col-md-6 offset-md-0 text-md-left fw-semibold"
+        >
+          <img
+            src="../assets/img/icon_5.png"
+            class="col-md-2 float-md-start mb-2 ms-md-0"
+            alt="..."
+          />
+          <strong>Tanpa ada masa tunggu,</strong> perlindungan yang di dapatkan
+          dimulai sejak polis aktif.
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="col-sm-6 col-md-5 col-lg-6 fw-semibold">
+          <img
+            src="../assets/img/icon_3.png"
+            class="col-md-2 float-md-end mb-2 ms-md-2"
+            alt="..."
+          />
+          Perlindungan <strong>Asuransi GRATIS</strong> akan diberikan kepada
+          pemegang kartu kredit yang mendaftar program AXA Care Protection
+          melauli pilihkartu.com
+        </div>
+        <div
+          class="col-sm-5 offset-sm-2 col-md-6 offset-md-0 text-md-left fw-semibold"
+        >
+          <img
+            src="../assets/img/icon_6.png"
+            class="col-md-2 float-md-start mb-2 ms-md-0"
+            alt="..."
+          />
+          <strong>Bukan penawran</strong> melainkan pemberian secara cuman-cuma
+          kepada nasabah ter-registrasi via Pilihkartu.com <br />
+          yang sudah memiliki kartu kredit.
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid">
+      <h3 class="fw-bold">
+        Tentang
+        <medium class="text">Axa Care Protection</medium>
+      </h3>
+      <p class="content fw-semibold">
+        Program Asuransi AXA Care Protaction merupakan program perlindungan
+        berupa asuransi kecelakaan dari asuransi AXA Life Indonesia.
+      </p>
+      <p class="subcontent fw-semibold">
+        Peserta yang lolos verifikasi akan mendapatkan benefit berupa uang
+        pertanggungan bila terjadi kecelakaan yang mengakibatkan meninggal
+        dunia.
+      </p>
+      <div class="d-grid gap-2 col-3 mx-auto align-content-center">
+        <br />
+        <button class="btn btn-primary btn-lg fw-bolder" type="button">
+          DAFTAR SEKARANG JUGA!
+        </button>
+      </div>
+    </div>
+
+    <h3 class="heading fw-bold">
+      Dapatkan Perlindungan
+      <medium class="text">Axa Care Protection</medium>
+      Sekarang
+    </h3>
+    <h6 align="center" class="fw-bolder">
+      Cukup Isi Data Diri Anda pada form dibawah ini
+    </h6>
+    <div class="container">
+      <form class="row align-items-center">
+        <div class="col-xl-5 mt-3 mb-3" align="right">
+          <b>Nama Lengkap</b>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <input type="text" class="form-width" style="width: 80%" />
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right">
+          <b>Tanggal Lahir (dd/mm/yy)</b>
+        </div>
+        <div class="flex col-xl-5 mt-3 mb-3">
+          <input
+            type="number"
+            class="form-width col-sm-2"
+            style="height: 34px; margin-right: 10px"
+            placeholder="dd"
+          />
+          <input
+            type="number"
+            class="form-width col-sm-2"
+            style="height: 34px; margin-right: 10px"
+            placeholder="mm"
+          />
+          <input
+            type="number"
+            class="form-width col-sm-2"
+            style="height: 34px"
+            placeholder="yyy"
+          />
+
+          <svg
+            style="margin-left: 10px; margin-bottom: 5px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+            width="20"
+            height="30"
+            fill="red"
+          >
+            <path
+              d="M160 32V64H288V32C288 14.33 302.3 0 320 0C337.7 0 352 14.33 352 32V64H400C426.5 64 448 85.49 448 112V160H0V112C0 85.49 21.49 64 48 64H96V32C96 14.33 110.3 0 128 0C145.7 0 160 14.33 160 32zM0 192H448V464C448 490.5 426.5 512 400 512H48C21.49 512 0 490.5 0 464V192zM64 304C64 312.8 71.16 320 80 320H112C120.8 320 128 312.8 128 304V272C128 263.2 120.8 256 112 256H80C71.16 256 64 263.2 64 272V304zM192 304C192 312.8 199.2 320 208 320H240C248.8 320 256 312.8 256 304V272C256 263.2 248.8 256 240 256H208C199.2 256 192 263.2 192 272V304zM336 256C327.2 256 320 263.2 320 272V304C320 312.8 327.2 320 336 320H368C376.8 320 384 312.8 384 304V272C384 263.2 376.8 256 368 256H336zM64 432C64 440.8 71.16 448 80 448H112C120.8 448 128 440.8 128 432V400C128 391.2 120.8 384 112 384H80C71.16 384 64 391.2 64 400V432zM208 384C199.2 384 192 391.2 192 400V432C192 440.8 199.2 448 208 448H240C248.8 448 256 440.8 256 432V400C256 391.2 248.8 384 240 384H208zM320 432C320 440.8 327.2 448 336 448H368C376.8 448 384 440.8 384 432V400C384 391.2 376.8 384 368 384H336C327.2 384 320 391.2 320 400V432z"
+            />
+          </svg>
+        </div>
+
+        <div class="col-xl-5 mt-3 mb-3" align="right">
+          <b>Email</b>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <input type="text" class="form-width" style="width: 80%" />
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right">
+          <b>Nomor Ponsel</b>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <input type="text" class="form-width" style="width: 80%" />
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right">
+          <b>Memiliki Kartu Kredit</b>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <input type="checkbox" id="visa" />
+          <label for="visa">VISA</label>
+          <input type="checkbox" id="mscard" />
+          <label for="mscard">MASTER CARD</label>
+          <input type="checkbox" id="bcard" />
+          <label for="bcard">BCA CARD</label>
+          <input type="checkbox" id="lain" />
+          <label for="lain">LAINNYA</label>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right"></div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <a href=""> Syarat & Ketentuan Aplikasi</a>
+        </div>
+
+        <div class="col-xl-5 mt-3 mb-3" align="right"></div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <input
+            class="pernyataan-check-input form-check-input"
+            type="checkbox"
+            value=""
+            id="defaultCheck1"
+          />
+          <label class="pernyataan form-check-label" for="defaultCheck1">
+            Saya mengetahui dan menyetujui syarat dan ketentuan diatas serta
+            bersedia dihubungi oleh PT.AXA Life Indonesia untuk mendapatkan
+            penjelasan tentang produk dan informasi lainnya
+          </label>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right"></div>
+        <div class="col-xl-5 mt-3 mb-3">
+          <button
+            class="btn btn-primary col-xl-6"
+            style="background-color: #26468f"
+            type="button"
+          >
+            Kirim
+          </button>
+        </div>
+        <div class="col-xl-5 mt-3 mb-3" align="right"></div>
+        <div class="text col-xl-5 mt-3 mb-3">
+          <p>
+            * Promo ini hanya berlaku bagi pemegang atau pemilik kartu kredit
+            Syarat dan ketentuan lain diatur sebagaimana tertulis pada bagian
+            syarat dan ketentuan. PT.AXA Life Indonesia terdaftar & diawasi oleh
+            Otoritas Jasa keuangan (OJK)
+          </p>
+        </div>
+      </form>
+    </div>
+
+    <!-- Akhir -->
+
+    <!-- Langkah -->
+    <section id="asuransi-section">
+      <div class="container-fluid">
+        <h3 class="heading fw-bold">
+          4 Langkah Mudah Mendapatkan Asuransi
+          <medium class="text">Axa Life Indonesia</medium>
+          di Pilihkartu
+        </h3>
+        <img
+          src="../assets/img/langkah.png"
+          class="urutan img-fluid"
+          alt="..."
+        />
+      </div>
+    </section>
+    <!-- Akhir Langkah -->
+
+    <!-- Footer -->
+    <footer class="d-flex align-items-center">
+      <div class="container">
+        <p class="copyright col-md-4 mb-0">
+          Copyright (c) 2017 Pilihkartucom and AXA All Rights Reserved
+        </p>
+      </div>
+    </footer>
+    <!-- Akhir Footer -->
+  </body>
 </template>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+:focus {
+  outline: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+  overflow-x: hidden;
+}
+
+body {
+  position: relative;
+  background-color: var(--bg-color);
+  color: var(--font-color);
+}
+
+.pembungkus {
+  position: relative;
+}
+
+#navig_bar {
+  background: #ebebeb none repeat scroll;
+  border-radius: 0;
+  padding: 2;
+}
+
+.navbar .btn {
+  --bs-btn-padding-y: 0.53rem;
+  --bs-btn-padding-x: 0.6rem;
+  --bs-btn-font-size: 0.85rem;
+  --bs-btn-border-color: transparent;
+  --bs-btn-margin-bottom: 2px;
+  --bs-btn-bg: #26468f;
+}
+
+#btn {
+  position: absolute;
+  right: 50px;
+  bottom: 90px;
+  letter-spacing: 2px;
+  background-color: #26468f;
+}
+
+.btn .bi {
+  word-spacing: 2rem;
+}
+
+.container .col-md-3 {
+  padding: 2px;
+  height: 47px;
+}
+
+h1,
+h4 {
+  margin: 5px;
+  color: #26468f;
+  text-indent: 228px;
+  word-spacing: 1.2px;
+}
+
+.smalltitle {
+  font-size: 1.1rem;
+}
+
+.bigtitle {
+  margin-top: -1rem;
+  font-size: 1.9rem;
+}
+
+.img-fluid {
+  padding-top: 5px;
+  padding-bottom: 15px;
+}
+
+h3 {
+  text-align: center;
+  padding-top: 12px;
+  padding-bottom: 50px;
+}
+
+.container .row {
+  font-size: 15px;
+  padding-bottom: 25px;
+  text-justify: newspaper;
+}
+
+.col-md-2 {
+  height: auto;
+  width: auto;
+}
+
+.text {
+  color: #26468f;
+}
+
+.container-fluid {
+  background: #f8f9fa;
+  margin-top: 2.5rem;
+  padding-top: 1.2rem;
+  padding-bottom: 3rem;
+}
+
+.content {
+  text-align: center;
+  font-size: small;
+  margin-top: -2rem;
+}
+
+.subcontent {
+  text-align: center;
+  font-size: small;
+  margin-top: -1rem;
+}
+
+.d-grid .btn {
+  --bs-btn-bg: #26468f;
+  --bs-btn-border-color: transparent;
+  box-shadow: #ced4da 7px 7px;
+}
+
+.heading {
+  padding-top: 2rem;
+}
+
+.flex {
+  font-size: 16px;
+}
+
+i {
+  color: #26468f;
+  margin-left: 15rem;
+}
+
+.terms-link {
+  color: #26468f;
+  font-size: 14px;
+  font-weight: 600 !important;
+}
+
+.pernyataan-check-input {
+  margin-top: 10px;
+  text-indent: 12px;
+}
+
+.btn {
+  --bs-btn-padding-y: 0.53rem;
+  --bs-btn-padding-x: 0.6rem;
+  --bs-btn-font-size: 0.85rem;
+  --bs-btn-border-color: transparent;
+  --bs-btn-margin-bottom: 2px;
+  --bs-btn-bg: #26468f;
+}
+
+.text p {
+  width: 600px;
+  margin-top: 0.5px;
+  font-size: 14px;
+  font-weight: 400;
+  font-style: italic;
+  color: #555555;
+}
+
+.urutan {
+  padding: 30px 90px 30px 90px;
+}
+
+footer {
+  width: auto;
+  height: 50px !important;
+  background-color: #063585;
+}
+
+.copyright {
+  font-size: 12.5px;
+  width: 550px;
+  color: #ffffff;
+}
+</style>
